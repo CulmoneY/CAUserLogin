3ae32d78a6d8064d581a61aa436371aadbb25322
CulmoneY <yculmone@rogers.com>
2024-10-21 17:56:43 -0400

    Task 2 - Checkstyle

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -35,6 +35,22 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
         return users.get(username);
     }
 
+    /**
+     * @param name ..
+     */
+    @Override
+    public void setCurrentUser(String name) {
+        this.currentUser = name;
+    }
+
+    /**
+     * @return .
+     */
+    @Override
+    public String getCurrentUser() {
+        return this.currentUser;
+    }
+
     @Override
     public void changePassword(User user) {
         // Replace the old entry with the new password
